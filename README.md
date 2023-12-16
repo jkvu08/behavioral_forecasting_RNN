25ded1b77026a302d058f0efb80164835c7f6c86
Jannet Vu <jannetkimvu@gmail.com>
2023-12-15 17:09:06 -0800

    update data description and visuals
    include more thorough description of input data and add time series visualization

--- README.md
+++ README.md
@@ -1,35 +1,20 @@
+# Multiclass behavioral classification
+Multi-class behavioral predictions using recurrent neural networks.
 
-# 5	Forecasting the activity of the black-and-white ruffed lemur (Varecia variegata) using recurrent neural networks
+# Files
+behavior-preprocessing.py: Data preprocessing steps 
+single_vanilla_rnn.py: Behavioral prediction using vanilla RNN model 
+single_ende_rnn.py: Behavioral prediction using encoder-decoder RNN model analyses
+hyperopt_vanilla_rnn.py: Hyperparameter optimization for the vanilla RNN behavioral forecasting model
+hyperopt_ende_rnn.py: Hyperparameter optimization for the encoder-decoder RNN behavioral forecasting model 
+hyperopt_visual_assess.py: Wrapper to visualize the hyperparameter optimization runs
+permutation_feat_importance.py: Permutation feature importance analysis
+null_models.py: Generate null model predictions and assess performance. Two null models were used in this study: 
+ - Null0 makes predictions by drawing from the behavior frequency distributions
+ - Null1 makes predictions by drawing based on the transition likelihoods between behaviors
 
-Predict the sub-daily multi-state behavior of wild black-and-white ruffed lemurs based on their internal and external conditions. 
-## Acknowledgements
- - [Almeida & Azkune 2018, Predicting human behaviour with recurrent neural networks](https://www.mdpi.com/2076-3417/8/2/305)
- - [Gomes et al. 2020, An Amazon stingless bee foraging activity predicted using recurrent artificial neural networks and attribute selection](https://www.nature.com/articles/s41598-019-56352-8)
- - [@Jason Brownlee](https://machinelearningmastery.com/)
- - [@Krish Naik](https://github.com/krishnaik06)
- 
+# Requirements
+Tensorflow
 
-
-## Authors
-
-- [Jannet K. Vu](https://www.github.com/jkvu08)
-- [Sheila M. Holmes](https://www.researchgate.net/profile/Sheila-Holmes)
-- [Steig E. Johnson](https://www.steigjohnson.com/)
-- [Edward E. Louis Jr.](https://www.researchgate.net/profile/Edward-Louis)
-
-
-
-## Content
-
-    1. behavior_preprocessing.py - Code for preprocessing data for behaviral prediction models 
-    2. hyperopt_vanilla_rnn.py - Code for optimizing vanilla RNN parameters using Bayesian hyperparameter optimization
-    3. hyperopt_ende_rnn.py - Code for optimizing encoder-decoder RNN parameters using Bayesian hyperparameter optimization
-    5. null_models.py - Code for behavioral prediction using Markov model and activity distribution model
-    6. permutation_feat_importance.py - Code for permutation feature importance to determine the relative contribution of each feature in each timestep on the behavioral prediction
-    7. single_ende_rnn.py - Code for behavioral prediction using encoder-decoder RNN
-    8. single_vanilla_rnn.py - Code for behavioral prediction using vanilla RNN
-    
-## Support
-
-For support, email jannetkimvu@ucla.edu.
-
+# Data
+Data used for study is not publically available, edit
--- behavior_preprocessing.py
+++ behavior_preprocessing.py
@@ -5,12 +5,14 @@ Data preprocessing for the behavioral prediction dataset
 1) Visualized raw data
 2) One-hot encoded the responses
 3) Transformed time covariates into cyclic covariates 
-4) Mormalized all covariates
+4) Normalized all covariates
 
 Input: Cleaned, unprocessed data
 Output: Processed data
 
 """
+# load packages
+import os
 import pandas as pd
 from pandas import read_csv
 import numpy as np
@@ -20,6 +22,74 @@ from sklearn.preprocessing import MinMaxScaler
 import os
 import multiprocessing as mp
 import seaborn as sns
+import itertools
+from matplotlib import pyplot
+
+# set working directory
+os.chdir("C:\\Users\\Jannet\\Documents\\Dissertation\\codes\\behavioral_forecasting_RNN")
+# set directory where results will be saved
+path = "C:\\Users\\Jannet\\Documents\\Dissertation\\codes\\behavioral_forecasting_RNN\\outputs\\"
+
+# import data
+dataset = read_csv('vv_lstm_data_2021_07_12.csv', header=0, index_col = 0)
+
+dataset.info()
+# Data columns (total 25 columns):
+#  #   Column                 Dtype         Description
+# ---  ------                 ----          -----  
+#  0   ID                     int64         record ID  
+#  1   TID                    int64         group track ID   
+#  2   focal                  object        focal ID    
+#  3   behavior               object        behavior (R-resting, F-feeding, S-socializing, T-traveling, UNK-Unknown)
+#  4   track_length           int64         no. of records in track   
+#  5   track_position         int64         track timestep of record  
+#  6   since_rest             float64       no. of timesteps since resting 
+#  7   since_feed             float64       no. of timesteps since feeding
+#  8   since_travel           float64       no. of timesteps since traveling
+#  9   since_social           float64       no. of timesteps since socializing 
+#  10  individual_continuity  int64         0/1 if same focal followed 
+#  11  sex                    float64       sex  
+#  12  adults                 float64       no. of adults in group
+#  13  infants                float64       no. of infants in group
+#  14  juveniles              float64       no. of juveniles in group
+#  15  reproductive_state     object        reproductive phase (GES-gestatation, LAC- lactation, NR- nonbreeding, MEG- mating/early gestation)
+#  16  rain                   float64       hourly rainfall (mm)
+#  17  temperature            float64       hourly temp (C)
+#  18  flower_count           float64       biweekly no. of trees flowering in phenology plots 
+#  19  fruit_count            float64       biweekly no. of trees fruiting in phenology plots 
+#  20  flower_shannon         float64       biweekly diversity of flowering trees in phenology plots 
+#  21  fruit_shannon          float64       biweekly diversity of fruiting trees in phenology plots 
+#  22  minutes                int64         minute of the day
+#  23  doy                    int64         doy of the year
+#  24  year                   int64         year
+
+# colums 0-2 are identifiers
+# columns 3 is the response 
+# column 4-24 are the predictors
+
+# Visualize time series of predictors
+name_list = ['rain', 'temperature', 'flower_count', 'fruit_count','flower_shannon','fruit_shannon',
+             'adults','infants', 'juveniles','since_rest','since_feed','since_travel',
+             'track_length','track_position']
+
+groups = list(itertools.chain(range(16,22), range(12,15),range(6,9),range(4,6)))
+
+# plot subsample of predictor timeseries
+fig, axs = pyplot.subplots(14,1,figsize=(3,5), sharex = True, sharey = False, dpi = 300)
+i = 1 # create counter
+for group in groups: # for each group
+    pyplot.subplot(len(groups), 1, i) # divide the plot space 
+    g = sns.lineplot(dataset.loc[12951:18173, 'doy'],dataset.iloc[12951:18173, group], ci = None, color = 'gray') # plot the covariates
+    g.locator_params(axis = 'y',nbins = 2, tight = None)
+    g.set_ylabel(dataset.columns[group], fontsize = 7, rotation = 40, ha='right')
+    g.tick_params(axis = 'both',labelsize = 7)
+    g.set_xlabel('day of year', fontsize = 8)
+    g.set(xlim = (100,315))
+    i += 1 # update counter 
+pyplot.show()
+
+fig.savefig(path+'data_timeseries_samples.png', dpi=150)
+
 
 ## one hot encode categorical data
 # decode a one hot encoded string
@@ -54,9 +124,8 @@ def cyclic_conversion(x, xmax):
   xtab = scaler.fit_transform(xtab) # scale the data
   return scaler, xtab
     
-# import dataframe
-# data was previously clean and standardized in R
-dataset = read_csv('data.csv', header=0, index_col = 0)
+# import data
+dataset = read_csv('vv_lstm_data.csv', header=0, index_col = 0)
 
 # convert string to numeric using factorization
 bcode = pd.factorize(dataset.behavior.values,na_sentinel=None, sort=True)
